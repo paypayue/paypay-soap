8c74c0783a9754b54ea3ef08455b04852933fcf2
Wilmer Gonçalves <wilmer.goncalves@acin.pt>
2017-08-01 14:19:27 +0100

    added structures

---
+++ src/structures/ResponseCheckWebPayment.php
@@ -0,0 +1,30 @@
+<?php
+namespace PayPay\Structures;
+
+class ResponseCheckWebPayment
+{
+    /** @var string */
+    public $errorCode;
+
+    /** @var string */
+    public $errorMessage;
+
+    /** @var string */
+    public $referenceEntity;
+
+    /** @var string */
+    public $referencia;
+
+    public function __construct(
+        $errorCode,
+        $errorMessage,
+        $referenceEntity,
+        $referencia
+    )
+    {
+        $this->referenceEntity = $referenceEntity;
+        $this->referencia      = $referencia;
+        $this->errorCode       = $errorCode;
+        $this->errorMessage    = $errorMessage;
+    }
+}
---
+++ src/structures/ResponseEntityPayments.php
@@ -0,0 +1,18 @@
+<?php
+namespace PayPay\Structures;
+
+class ResponseEntityPayments
+{
+    /** @var ResponseIntegrationState */
+    public $integrationState;
+
+    /** @var ResponsePayment[] */
+    public $payments;
+
+    public function __construct($state)
+    {
+        $this->integrationState = $state;
+        $this->payments = array();
+    }
+
+}
