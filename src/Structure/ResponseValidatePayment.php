2462f0f1946a9c1cc87b63af38846055111c0a88
PayPayUE <apoio@paypay.pt>
2018-06-04 10:22:00 +0100

    added method validatePaymentReference

--- src/Structure/ResponseValidatePayment.php
+++ src/Structure/ResponseValidatePayment.php
@@ -1,25 +1,30 @@
 <?php
 namespace PayPay\Structure;
 
-class ResponseValidatePayment {
-
+class ResponseValidatePayment
+{
     /** @var ResponseIntegrationState */
     public $integrationState;
 
     /** @var ResponsePaymentOption[] */
     public $paymentOptions;
 
+    /** @var string */
+    public $err_msg;
+
+    /** @var string */
+    public $err_code;
 
     public function __construct($params)
     {
         $fields = array(
-                        'integrationState',
-                        'paymentOptions');
+            'integrationState',
+            'paymentOptions',
+            'err_code',
+            'err_msg'
+        );
         foreach ($fields as $fkey => $fvalue) {
-            $this->$fvalue = empty($params[$fvalue]) ? '': $params[$fvalue];
+            $this->$fvalue = empty($params[$fvalue]) ? '' : $params[$fvalue];
         }
     }
-
 }
-
-?>
